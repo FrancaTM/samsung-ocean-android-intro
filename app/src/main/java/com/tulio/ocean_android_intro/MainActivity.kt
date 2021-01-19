eeb756164bb25d50582c752e9b3d59e6641a9b55
FrancaTM <francatm@outlook.com>
2021-01-19 15:06:50 -0300

    implement etName behavior

--- app/src/main/java/com/tulio/ocean_android_intro/MainActivity.kt
+++ app/src/main/java/com/tulio/ocean_android_intro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tulio.ocean_android_intro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,14 @@ class MainActivity : AppCompatActivity() {
 
         val tvOutput = findViewById<TextView>(R.id.tvOutput)
         val btSend = findViewById<Button>(R.id.btSend)
+        val etName = findViewById<EditText>(R.id.etName)
 
-        btSend.setOnClickListener { tvOutput.text = "Olá, Túlio!" }
+        btSend.setOnClickListener {""
+            if (etName.text.isNotBlank()) {
+                tvOutput.text = "Olá, ${etName.text}!"
+            } else {
+                etName.error = "Digite um nome..."
+            }
+        }
     }
 }
