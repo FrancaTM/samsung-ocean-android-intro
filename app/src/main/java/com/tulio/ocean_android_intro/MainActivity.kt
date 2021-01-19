c90fe41fa16088407081f78c44e2c39bd2c24443
FrancaTM <francatm@outlook.com>
2021-01-18 23:49:41 -0300

    textView manipulation through activity code

--- app/src/main/java/com/tulio/ocean_android_intro/MainActivity.kt
+++ app/src/main/java/com/tulio/ocean_android_intro/MainActivity.kt
@@ -2,10 +2,15 @@ package com.tulio.ocean_android_intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvOutput = findViewById<TextView>(R.id.tvOutput)
+
+        tvOutput.text = "Olá, Túlio!"
     }
 }
